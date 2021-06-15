ac8c0bf0548975e9ea7ebfb4d7b97dd5c1f248e2
VladislavArzamastsev <arzik66677@gmail.com>
2021-06-15 09:19:18 +0600

    Created algorithm for counting substrings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <algorithm>
+
+int countSubstring(const std::string& s, const std::string& target){
+    int occurrences = 0;
+    std::string::size_type pos = 0;
+    while ((pos = s.find(target, pos )) != std::string::npos) {
+        ++ occurrences;
+        pos += target.length();
+    }
+    return occurrences;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Input your word:" << std::endl;
+    std::string input;
+    std::cin>>input;
+    std::cout << input << std::endl;
+    std::cout << countSubstring(input, "z")<<std::endl;
+    std::cout << countSubstring(input, "zz")<<std::endl;
     return 0;
 }
