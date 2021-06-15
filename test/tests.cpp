131a840626642aac1416cd2fb5a3e24b3a55b788
VladislavArzamastsev <arzik66677@gmail.com>
2021-06-15 09:32:05 +0600

    Added tests for counter

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,8 +1,21 @@
 #define CATCH_CONFIG_MAIN
 
 #include "../catch.hpp"
+#include "../counter/SubsequenceCounter.h"
+#include "../counter/SimpleSubsequenceCounter.h"
 
 TEST_CASE(){
+    SubsequenceCounter* counter = new SimpleSubsequenceCounter;
+    REQUIRE(counter->countSubstring("fuzzyzy", "z") == 3);
+}
 
+TEST_CASE(){
+    SubsequenceCounter* counter = new SimpleSubsequenceCounter;
+    REQUIRE(counter->countSubstring("fuzzyzy", "zz") == 1);
+}
+
+TEST_CASE(){
+    SubsequenceCounter* counter = new SimpleSubsequenceCounter;
+    REQUIRE(counter->countSubstring("aaaa", "z") == 0);
 }
 
